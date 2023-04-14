a95b07702f068719b9e6b9137f73048a0ad5b05e
Dustin Alandzes <fezf00@gmail.com>
2023-04-14 12:19:15 -0500

    add base url and vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/super-mario-64-star-select/",
+  plugins: [react(), checker({
+        // e.g. use TypeScript check
+        typescript: true,
+    })],
 })
